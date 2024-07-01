062dd0d83ca106df3dc0ed319360eb0be65c0012
serhii.lytvynov <serhii.lytvynov@onix-systems.com>
2024-07-01 16:48:51 +0300

    init

---
+++ src/main/java/com/example/sonarpipeline/SonarPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.example.sonarpipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarPipelineApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SonarPipelineApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/sonarpipeline/SonarPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sonarpipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarPipelineApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
